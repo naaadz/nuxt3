f09351b918bfc11dd1fdbe942c5c7d11f1bd9ca9
DESKTOP-8S4L8MM\Nadia <nstoffel@sitezeus.com>
2022-08-02 13:58:28 -0400

    hooked up tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,21 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	// ssr: false,
+	buildModules: [
+	],
 	vite: {
         css: {
 			devSourcemap: true,
         }
     },
 	css: [
-		'~/assets/styles.scss',
-		'~/assets/test.scss',
-	]
+		'@/assets/styles.scss',
+		'@/assets/test.scss',
+		'@/assets/css/tailwind.css'
+	],
+	build: {
+		postcss: {
+		  postcssOptions: require('./postcss.config.js'),
+		},
+	  }
 })
