4027a78af254f89733a667c449136fd2f09b28a5
DESKTOP-8S4L8MM\Nadia <nstoffel@sitezeus.com>
2022-08-02 12:27:01 -0400

    add sourcemaps to vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	// ssr: false,
+	vite: {
+        css: {
+			devSourcemap: true,
+        }
+    },
 	css: [
 		'~/assets/styles.scss',
 		'~/assets/test.scss',
